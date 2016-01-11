bdcf8bf637075cd9b34e2914e07239d5bd69e1b9
Tyler Florez <qualiabyte@gmail.com>
2016-01-11 04:37:40 -0800

    Add script for portfolio data

---
+++ js/data.js
@@ -0,0 +1,93 @@
+var current = [
+  {
+    name: "Mukei Dolphin",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/mukei-dolphin",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Uta Diary",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/uta-nikki",
+    tools: [],
+    tags: []
+  }
+];
+
+var portfolio = [
+  {
+    name: "Argumenta",
+    desc: "",
+    image: "",
+    url: "http://blog.argumenta.io",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Worldlines",
+    desc: "",
+    image: "",
+    url: "https://worldlines.sourceforge.net",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Coinlens",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/coinlens",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Open Source Economy",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/open-source-economy",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Super Satoshi Bros",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/super-satoshi-bros",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Creamscript",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/creamscript",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Ethereum Contracts",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/ethereum-contracts",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Postgres Nest Transaction",
+    desc: "",
+    image: "",
+    url: "https://github.com/qualiabyte/pg-nest-transaction",
+    tools: [],
+    tags: []
+  },
+  {
+    name: "Sweeten Docco",
+    desc: "Sweetens <a href=\"https://jashkenas.github.io/docco/\">docco</a> output with syntax highlighted JSDoc and Codo tags.",
+    image: "",
+    url: "https://github.com/qualiabyte/sweeten-docco",
+    tools: [],
+    tags: []
+  }
+];
